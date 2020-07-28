7b7f63b3a15c6d37c0e539d5ea9747ef202781d3
Patrick Baldwin <patrickbaldwin614@gmail.com>
2020-07-27 21:56:59 -0400

    togglable cells

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Board from './components/Board';
 
 import './App.scss';
 
 function App() {
+  const [paused, setPaused] = useState(true);
+  const [width] = useState(25);
+  const [height] = useState(25);
+
+  const boardProps = { width, height, paused };
+
   return (
-    <Board />
+    <Board {...boardProps} />
   );
 }
 
--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
+
+import { List } from 'immutable';
 
 import './Board.scss';
 
-function Board() {
+type BoardProps = {
+  paused: boolean,
+  width: number,
+  height: number,
+}
+
+function Board({ width, height, paused }: BoardProps) {
+  const [grid, setGrid] = useState(List<boolean>(Array(width * height).fill(false)));
+
   return (
     <div className="board">
-      {Array(625).fill(true).map(() => {
-        return (<div className="cell on" />);
-      })}
+      {grid.map((alive, index) => (
+        <div
+          className={`cell ${alive ? 'alive' : ''}`}
+          onClick={() => {
+            if (!paused) return;
+            setGrid((oldGridState) => (oldGridState.set(index, !oldGridState.get(index))));
+          }}
+        />
+      ))}
     </div>
   );
 }
